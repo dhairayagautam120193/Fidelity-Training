946635e1a92c8d5e0df52b1b89e487ec7f0f0276
Dhairaya Gautam <dhairayagautam1@gmail.com>
2020-08-31 17:04:12 +0530

    Example5

---
+++ Day9/Example5/Main.java
@@ -0,0 +1,13 @@
+package Example5;
+
+public class Main {
+	public static void main(String args[]) {
+		
+		BoxArray BA = new BoxArray();
+	
+		int toatlVolume = BA.getTotalVolume();
+	
+		System.out.println(toatlVolume);
+	}
+
+}
