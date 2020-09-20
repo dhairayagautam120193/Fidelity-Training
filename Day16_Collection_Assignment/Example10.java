782e0ce89f5c559054064fbaf8f32516db6518d3
Dhairaya Gautam <dhairayagautam1@gmail.com>
2020-09-20 20:32:18 +0530

    Example10

---
+++ Day16_Collection_Assignment/Example10.java
@@ -0,0 +1,56 @@
+package collectionAssignment;
+
+import java.util.ArrayList;
+
+public class Example10 {
+		
+		// Write a Java program to join two array lists
+		
+		public static void main(String[] args) {
+			
+		ArrayList<String> list1= new ArrayList<String>();
+		
+		list1.add("Infosys");
+		list1.add("TCS");
+		list1.add("Wipro");
+		list1.add("cognizant");
+		
+		System.out.println("First list....");
+		
+		for(String str:list1){
+			
+			System.out.println(str);
+			
+		}
+		System.out.println();
+		ArrayList<String> list2= new ArrayList<String>();
+		
+		list2.add("Google");
+		list2.add("Microsoft");
+		list2.add("Twitter");
+		list2.add("Facebook");
+		
+		System.out.println("2nd list.....");
+		
+		for(String str1:list2){
+			
+			System.out.println(str1);
+			
+		}
+			
+		
+		list2.addAll(list1);
+		System.out.println();
+		System.out.println("After copying first list into another list....");
+		
+		for(String str2:list2){
+			
+			System.out.println(str2);
+		}
+		
+		
+		}
+		
+	
+
+}
